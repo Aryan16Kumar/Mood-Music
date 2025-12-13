9b3c0f5d5b5684b69e7b2111119c2851f0ac27b2
Aryan16Kumar <Aryan1612Kumar@gmail.com>
2025-12-13 11:31:51 +0530

    Added the functionality of play pause next and prev

--- script.js
+++ script.js
@@ -111,6 +111,60 @@ function loadSong(index) {
     songTitle.textContent = song.title;
 }
 
+function togglePlay() {
+    //check if mood is selected
+    if (currentMood === null) {
+        alert('Please select a mood first');
+        return;
+    }
+
+    //If music is playing, Pause it
+    if(isPlaying) {
+        audioPlayer.pause();
+        playBtn.textContent = '▶️' ;
+    }
+
+    //If musics is paused, play it
+    else {
+        audioPlayer.play();
+        playBtn.textContent = '⏸️';
+    }
+
+    //toggle isPlaying state
+    isPlaying = !isPlaying
+}
+
+function nextSong() {
+    //check if Playlist exist
+    if(currentPlaylist.length === 0) return;
+
+    //move to next song (if at end go back to 0)
+    currentSongIndex = (currentSongIndex + 1) % currentPlaylist.length;
+
+    //Load the new song
+    loadSong(currentSongIndex);
+
+    //if music was playing keep playing
+    if (isPlaying) {
+        audioPlayer.play();
+    }
+}
+
+function prevSong() {
+    if(currentPlaylist.length === 0) return;
+
+    //move to prevSong
+    currentSongIndex = (currentSongIndex - 1 + currentPlaylist.length) % currentPlaylist.length;
+
+    //load new song
+    loadSong(currentSongIndex);
+
+    if(isPlaying) {
+        audioPlayer.play();
+    }
+} 
+
+//EVENT LISTENERS
 //Listens for clicks on mood buttons
 moodButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -124,4 +178,9 @@ moodButtons.forEach(button => {
         moodButtons.forEach(btn => btn.classList.remove('active'));
         button.classList.add('active');
     });
+
+    playBtn.addEventListener('click', togglePlay);
+    nextBtn.addEventListener('click', nextSong);
+    prevBtn.addEventListener('click', prevSong);
+    
 });
